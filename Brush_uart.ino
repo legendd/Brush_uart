d2778dcccd6072b80b8feb24e08068c1baba89f5
legendd <legendshiao@hotmail.com>
2015-07-29 14:58:39 +0800

    Unable UART ISR,Use SerialEvent

--- Brush_uart.ino
+++ Brush_uart.ino
@@ -5,30 +5,32 @@ pin                 2        3        21      20      19      18
  * USART_RXinterrupt.pde: 使用 USART 接收資料&#65292;中斷版本
  */
  /*  I2C
-     Mega2560	20 (SDA), 21 (SCL)   ;  UNO     A4(SDA), A5(SCL)
+     Mega2560  20 (SDA), 21 (SCL)   ;  UNO     A4(SDA), A5(SCL)
  */
 #include <Hx711.h>
 #include <Wire.h> //I2C
-#define SLAVE1_ID 1
+
  
 Hx711 scale_1(A1, A0);    //set analog pins
 Hx711 scale_2(A3, A2);    //set analog pins
 //Hx711 scale_3(A5, A4);    //set analog pins
 int a = 0;
 int brushtime = 3000;
+char incomingByte = 0;
+const int SLAVE_ADDRESS = 1;
 /* #1 */
-const int Brush1_In1 = 30;  // brush #1
-const int Brush1_In2 = 32;  // brush #1
-const int Motor1_In3 = 34; // motor #1
-const int Motor1_In4 = 36; // motor #1
+const int Brush1_In1 = 22;  // brush #1
+const int Brush1_In2 = 24;  // brush #1
+const int Motor1_In3 = 26; // motor #1
+const int Motor1_In4 = 28; // motor #1
 const int EN1_A = 4;      // Enable Brush(#1)
 const int EN1_B = 5;      // Enable motor(#1)
 const int led = 10;
 /* #2 */
-const int Brush2_In1 = 22; // brush #2
-const int Brush2_In2 = 24; // brush #2
-const int Motor2_In3 = 26; // motor #2
-const int Motor2_In4 = 28; // motor #2
+const int Brush2_In1 = 30; // brush #2
+const int Brush2_In2 = 32; // brush #2
+const int Motor2_In3 = 34; // motor #2
+const int Motor2_In4 = 36; // motor #2
 const int EN2_A = 6;     // Enable Brush(#2)
 const int EN2_B = 7;     // Enable motor(#2)
 /*IR MODULE*/
@@ -41,7 +43,8 @@ int readybit2 = 0;
 volatile int buttonState; 
 void setup() {
   /* Set USART0 Interrupt */
-  mySerial_begin(9600);  
+  //mySerial_begin(9600);  
+  Serial.begin(9600);
   /* Set I2C, Communication Between 2 MEGA */
   Wire.begin();        // join i2c bus (address optional for master)
   pinMode(led, OUTPUT);
@@ -72,11 +75,11 @@ void setup() {
   EICRA |= _BV(ISC00);
   */
   /* #1 Motor Set Limit Switch*/
-  attachInterrupt(0, Touchfront1 , LOW); //INT 0 in PIN 2
-  attachInterrupt(1, Touchfront2, LOW);  //INT 1 in PIN 3
+  //attachInterrupt(0, Touchfront1 , LOW); //INT 0 in PIN 2
+  //attachInterrupt(1, Touchfront2, LOW);  //INT 1 in PIN 3
   /* #2 Motor Set Limit Switch*/
-  attachInterrupt(4, Touchback1, LOW);   //INT 4 in PIN 19
-  attachInterrupt(5, Touchback2, LOW);   //INT 5 in PIN 18
+  //attachInterrupt(4, Touchback1, LOW);   //INT 4 in PIN 19
+  //attachInterrupt(5, Touchback2, LOW);   //INT 5 in PIN 18
 }
 
 void loop() {
@@ -93,8 +96,10 @@ void loop() {
     }
   }
   */
+  
   if(a == 1){
     digitalWrite(led, HIGH);
+    Serial.println("11111");
     /*想一下master傳給slave的UART指令要放在哪裡*/
     float result1 = scale_1.getGram();  /*1kg strain gauge*/
     float result2 = scale_2.getGram();  /*1kg strain gauge*/
@@ -150,27 +155,38 @@ void buttonStateChanged() {
 ISR(INT0_vect){
   buttonStateChanged();
 }*/
-
+/*
 ISR(USART0_RX_vect)
 {
   char ReceivedChar;
   ReceivedChar  = UDR0;
   if(ReceivedChar=='1'){
   UDR0 = ReceivedChar;
-  Wire.beginTransmission(SLAVE1_ID); // transmit to device #1
-  Wire.write('1');              // sends one byte  
-  Wire.endTransmission();    // stop transmitting
   a = 1;
   }
   else if(ReceivedChar=='0'){
   UDR0 = ReceivedChar;
-  Wire.beginTransmission(SLAVE1_ID); // transmit to device #1
-  Wire.write('0');              // sends one byte  
-  Wire.endTransmission();    // stop transmitting
   a = 0;
   }
   
+}*/
+void serialEvent()
+{
+  incomingByte = Serial.read();
+  if(incomingByte == '1'){
+    Wire.beginTransmission(SLAVE_ADDRESS); // transmit to device #1
+    Wire.write(incomingByte);              // sends one byte  
+    Wire.endTransmission();    // stop transmitting
+    a = 1;
+  }
+  else if(incomingByte == '0'){
+    Wire.beginTransmission(SLAVE_ADDRESS); // transmit to device #1
+    Wire.write(incomingByte);              // sends one byte  
+    Wire.endTransmission();    // stop transmitting
+  a = 0;
+  }    
 }
+
 void led_HIGH(){
   digitalWrite(led, HIGH);
   delay(5000);
@@ -221,38 +237,30 @@ void motorstop(int Motor1, int Motor2){
   digitalWrite(Motor2, LOW);
   delay(50);
 }
+/*Interrupt Function*/
 void Touchfront1(){
   motorstop(Motor1_In3, Motor1_In4);
   delay(50);
   backward(Motor1_In3, Motor1_In4);
-  digitalWrite(38, HIGH);
   delay(5000);
-  //Serial.println("INTR");
 }
 
-/*Interrupt Function*/
 void Touchback1(){
   motorstop(Motor1_In3, Motor1_In4);
   delay(50);
-  digitalWrite(40, HIGH);
-  delay(5000);
   forward(Motor1_In3, Motor1_In4);
-  //Serial.println("INTR");
+  delay(5000);
 }
 void Touchfront2(){
   motorstop(Motor2_In3, Motor2_In4);
   delay(50);
-  digitalWrite(42, HIGH);
-  delay(5000);
   backward(Motor2_In3, Motor2_In4);
-   //Serial.println("INTR");
+  delay(5000);
 }
 
 void Touchback2(){
   motorstop(Motor2_In3, Motor2_In4);
   delay(50);
-  digitalWrite(44, HIGH);
-  delay(5000);
   forward(Motor2_In3, Motor2_In4);
-   //Serial.println("INTR");
+  delay(5000);
 }
